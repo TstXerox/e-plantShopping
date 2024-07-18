a22a19f970a95cd7bb65760ed1c87598401cac0f
TstXerox <jouahriadib+github@gmail.com>
2024-07-18 13:54:49 +0200

    Fixing cart styles for smaller screens

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -235,7 +235,7 @@ function ProductList() {
 		display: "flex",
 		justifyContent: "space-between",
 		alignItems: "center",
-		width: "1100px",
+		width: "70%",
 	};
 	const styleA = {
 		color: "white",
