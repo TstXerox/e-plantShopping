36d3893dcd5b2c71e1a2dd027e27268103afcb2f
TstXerox <jouahriadib+github@gmail.com>
2024-07-18 12:01:42 +0200

    Adding the layout for the plants

--- src/CreateSlice.jsx
+++ src/CreateSlice.jsx
@@ -1,21 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CreatSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
+	name: "cart",
+	initialState: {
+		items: [], // Initialize items as an empty array
+	},
+	reducers: {
+		addItem: (state, action) => {},
+		removeItem: (state, action) => {},
+		updateQuantity: (state, action) => {},
+	},
 });
 
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
